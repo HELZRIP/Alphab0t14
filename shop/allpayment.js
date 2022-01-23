915e6a2525a18f75ce1fd1cd09ecc169bfe63781
Dream Garden (rey) <93763859+HELZRIP@users.noreply.github.com>
2022-01-24 05:57:00 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -3,29 +3,15 @@ return `PAYMENT BY ZEEONE OFC
 
 *🏧 BANK*
 
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
+Kagak ade
 	
 *💰 E-MONEY*
 
-1. GOPAY
-	ㅁ 0887 4350 47326
-2. OVO
-	ㅁ 0887 4350 47326
-3. DANA
-	ㅁ 0887 4350 47326
-	
-Semuanya atas nama RI****DI
+Kagak ade
 
 *📲 PULSA*
 
-Tidak tersedia
+➰ telkomsel: +6281232174604
 	
 Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
 `
