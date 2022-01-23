a38b59ca0a9ea1f5d2521d7a5fe2cbb143443025
Dream Garden (rey) <93763859+HELZRIP@users.noreply.github.com>
2022-01-24 05:59:48 +0700

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -3,25 +3,15 @@ return `DONASI BISA VIA BANK / E-MONEY
 
 *🏧 BANK*
 
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
+Kgk ade
 	
 *💰 E-MONEY*
 
-1. GOPAY
-	ㅁ 0887 4350 47326
-2. OVO
-	ㅁ 0887 4350 47326
-3. DANA
-	ㅁ 0887 4350 47326
-	
-Semuanya atas nama RI****DI
+Kgk ade
+
+*📱 PULSA*
+
+➰ Telkomsel: +6281232174604
 `
 	}
 
